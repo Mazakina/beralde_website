a40cc49fe5547f8bd5c2c88122f05f84d53a93a4
Mazakina <8.mazakina@gmail.com>
2025-05-06 18:52:26 -0300

    feat: replace beralde icons

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero(){
                 <Image src={hero} alt={""} layout="fill" className={`object-cover transition-all duration-700 absolute w-full ${isHovered? "brightness-90": "brightness-50"} `} style={{ objectPosition: "center 50%" }} />
             <div className="z-10 relative max-w-4/5 ">
                 <h1 className={`z-10 text-5xl md:text-6xl mb-8 transition-all duration-700 ${isHovered? "opacity-0": "opacity-100"}`}>As <span className="font-bold">Melhores verduras</span> para seu negócio</h1>
-                <p className={`mb-4 text-xl md:text-2xl transition-all duration-700 ${isHovered? "opacity-0": "opacity-100"}`}>Distribuímos os melhores vegetais da região, do campo direto para você!</p>
+                <p className={`mb-4 text-xl md:text-2xl transition-all duration-700 ${isHovered? "opacity-0": "opacity-100"}`}>Distribuímos as melhores hortaliças da região, do campo direto para você!</p>
                 <p className={`md:mb-18 mb-6 text-xl md:text-2xl transition-all duration-700 ${isHovered? "opacity-0": "opacity-100"}`}>Trabalhamos com o mesmo cuidado que dedicamos à nossa própria mesa.</p>
                 <Link
                 href={"https://w.app/beralde"} 
